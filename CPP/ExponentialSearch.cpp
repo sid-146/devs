c88bc1721ee9c9991728a3e9d8ab4fe72153a61c
Sudhanwa Kaveeshwar <sudhanwa.kaveeshwar@gamil.com>
2022-02-25 11:16:15 +0530

    Added

---
+++ CPP/ExponentialSearch.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void exponentialSearch()
+{
+}
+
+int BinarySearch()
+{
+    int index;
+    return index;
+}
+
+int main()
+{
+    int n = 10;
+    int arr[10];
+    return 0;
+}
