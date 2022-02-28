0fce89d009387ee26c31ebab80ba6db4849eb00e
Sudhanwa Kaveeshwar <sudhanwa.kaveeshwar@gamil.com>
2022-02-28 13:17:27 +0530

    Update ExponentialSearch.cpp

--- CPP/ExponentialSearch.cpp
+++ CPP/ExponentialSearch.cpp
@@ -4,14 +4,31 @@
 */
 
 #include <iostream>
+#include <bits/stdc++.h>
+#include <tuple>
 
 using namespace std;
 
-void exponentialSearch()
+int exponentialSearch(int arr[], int n, int value)
 {
+
+    int indexValue, low = 0, high = n - 1, exp = 1, prevExp = 0;
+
+    if (arr[low] == low)
+    {
+        return low;
+    }
+    if (arr[exp] > value)
+    {
+        low =
+    }
+
+    indexValue = BinarySearch(arr, n, low, high);
+
+    return indexValue;
 }
 
-int BinarySearch()
+int BinarySearch(int arr[], int n, int low, int high)
 {
     int index;
     return index;
@@ -22,11 +39,21 @@ int main()
     int n = 10;
     int arr[10];
 
+    // int value = 105;
+    int value = 100;
+
     for (int i = 1; i < n; i++)
     {
         arr[i] = i * 15;
     }
-    char a[4] = {'a', 'c', 'e', 'f'};
 
+    if (int ans = exponentialSearch(arr, n, value) == -1)
+    {
+        cout << "Value not in array" << endl;
+    }
+    else
+    {
+        cout << "value at " << ans << " index" << endl;
+    }
     return 0;
 }
