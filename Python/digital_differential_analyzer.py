a8eba850dce5d228709538bd2c73ae2412ba4363
Sudhanwa Kaveeshwar <sudhanwa.kaveeshwar@gamil.com>
2022-03-28 17:18:59 +0530

    Create digital_differential_analyzer.py

---
+++ Python/digital_differential_analyzer.py
@@ -0,0 +1,27 @@
+
+'''
+calculate dx, dy, slop
+
+
+'''
+from turtle import xcor
+from matplotlib import pyplot
+
+xCordinate = []
+yCordinate = []
+
+x1 = int(input("Enter Starting 'X' co-ordinate: "))
+y1 = int(input("Enter Starting 'Y' co-ordinate: "))
+
+xEnd = int(input("Enter ending 'X' co-ordinate: "))
+yEnd = int(input("Enter ending 'Y' co-ordinate: "))
+
+dx = xEnd - x1
+dy = yEnd - y1
+
+
+xCordinate = []
+yCordinate = []
+
+pyplot.plot(xCordinate, yCordinate)
+pyplot.show()
