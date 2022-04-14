c42b9de0b9d32bce1f3635c295458f506f5f5f0c
Sudhanwa Kaveeshwar <sudhanwa.kaveeshwar@gamil.com>
2022-04-14 15:12:36 +0530

    Update digital_differential_analyzer.py

--- Python/digital_differential_analyzer.py
+++ Python/digital_differential_analyzer.py
@@ -1,39 +1,27 @@
 
-# '''
-# calculate dx, dy, slop
+'''
+calculate dx, dy, slop
 
 
-# '''
-# from turtle import xcor
-# from matplotlib import pyplot
+'''
+from turtle import xcor
+from matplotlib import pyplot
 
-# xCordinate = []
-# yCordinate = []
+xCordinate = []
+yCordinate = []
 
-# x1 = int(input("Enter Starting 'X' co-ordinate: "))
-# y1 = int(input("Enter Starting 'Y' co-ordinate: "))
+x1 = int(input("Enter Starting 'X' co-ordinate: "))
+y1 = int(input("Enter Starting 'Y' co-ordinate: "))
 
-# xEnd = int(input("Enter ending 'X' co-ordinate: "))
-# yEnd = int(input("Enter ending 'Y' co-ordinate: "))
+xEnd = int(input("Enter ending 'X' co-ordinate: "))
+yEnd = int(input("Enter ending 'Y' co-ordinate: "))
 
-# dx = xEnd - x1
-# dy = yEnd - y1
+dx = xEnd - x1
+dy = yEnd - y1
 
 
-# xCordinate = []
-# yCordinate = []
+xCordinate = []
+yCordinate = []
 
-# pyplot.plot(xCordinate, yCordinate)
-# pyplot.show()
-
-testcase = int(input())
-
-while(testcase):
-    n = int(input())
-    ans = 'peace:)'
-    li = list(map(int, input().split()))
-    li.sort(reverse = True)
-    if li[0] == li[1]:
-        ans = 'fight:('
-    print(ans)
-    testcase -= 1
+pyplot.plot(xCordinate, yCordinate)
+pyplot.show()
