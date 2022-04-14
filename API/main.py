5ddf5be372febc32a632351ef900c8e1c219fd68
Sudhanwa Kaveeshwar <sudhanwa.kaveeshwar@gamil.com>
2022-04-14 23:14:15 +0530

    update

--- API/main.py
+++ API/main.py
@@ -1,26 +1,50 @@
-from email import message
 from flask import Flask, jsonify
-from flask_restful import Api, Resource, abort, reqparse
-from requests import request
+from flask_restful import Api, Resource, abort, reqparse, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
+
+
 
 ##! Error CODE
 '''
 # ! 200 working fine
 # ! 201 Successful updatation
 # ! 204 successful deletion
-# ! 
+# ! 409 aleardy exiting ID
+# !
 '''
 
 
 #create app
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///APIdatabase.db'
+
+db = SQLAlchemy(app) 
+
+class videoModel(db.Model):
+    #! Define all the fields you wnt in your database
+    ID = db.Column(db.Integer, primary_key = True)
+    Name = db.Column(db.String(100), nullable = False)
+    Views = db.Column(db.Integer, nullable = False)
+    Likes = db.Column(db.Integer, nullable = False)
+    
+    def __repr__(self):
+        return f"Video(name={Name}, views={Views}, likes={Likes})"
+
+
+# ! line number 35 should be untouched othewise it will mess up with the databse
+# db.create_all() #! DO NOT TOUCH IT !!!!!
+
+Resource_Field = {
+    "Name" : fields.String,
+    "Likes" : fields.Integer,
+    "Views" : fields.Integer,
+    "ID": fields.Integer
+}
 
 videos_request_parser = reqparse.RequestParser()
-
 # Cumpolsary and will produce error
 videos_request_parser.add_argument("name", type= str, help="Name of video", required = True) 
-
 # not cumpusolry requied (will not produce any error)
 # Values which are not provided are given as None
 videos_request_parser.add_argument("likes", type= int, help = "likes on the video")
@@ -45,6 +69,12 @@ def videoIDnotValid(video_id):
     if video_id not in videos:
         abort(404,message= "Video ID not present...")
 
+def videoIDExist(video_id):
+    if video_id in videos:
+        abort(409, message = "Video ID already exist")
+
+
+
 # Resourse Helps in handling requests (GET, POST, PUT etc)
 class HomePage(Resource):
     def get(self):
@@ -68,13 +98,26 @@ class Video(Resource):
         return videos[video_id]
 
     def put(self, video_id):
+        videoIDExist(video_id=video_id)
         args = videos_request_parser.parse_args()
         videos[video_id] = args
         return args, 201 # 201 showing successful updataion
 
+    def delete(self, video_id):
+        videoIDnotValid(video_id=video_id)
+        del videos[video_id]
+        return 204, " "
+        pass
+
+class NewVideoAPI(Resource):
+    def get(self, video_id):
+        result = videoModel.query.get(id= video_id)
+
+
+
+
 
 ### API RESOURCES ###
-
 
 api.add_resource(Video, "/Video/<int:video_id>")
 api.add_resource(HomePage, "/HP")
