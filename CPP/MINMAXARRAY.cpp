547c8f98560e866da800163b19a0f9007d70a47d
Sudhanwa Kaveeshwar <sudhanwa.kaveeshwar@gamil.com>
2022-07-13 22:38:13 +0530

    Simple Fix for MinMax file

--- CPP/MINMAXARRAY.cpp
+++ CPP/MINMAXARRAY.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
-struct minMax
+//? Created this for returing 2 values for single function
+struct Values
 {
     int MinValue;
     int MaxValue;
 };
 
+Values getMinMax(int len, int arr[])
+{
+    struct Values MinMax;
+    if (len == 0)
+    {
+        MinMax.MaxValue = arr[0];
+        MinMax.MinValue = arr[0];
+
+        return MinMax;
+    }
+
+    for (int i = 0; i < len; i++)
+    {
+    }
+
+    return MinMax;
+}
 
 int main()
 {
