0a178076897ce55edced81647545dd5d4c397bcc
Sudhanwa Kaveeshwar <sudhanwa.kaveeshwar@gamil.com>
2022-07-24 23:55:17 +0530

    Porblem added

---
+++ ProgramingQuestions/Python/alternating_subarray_prefix.py
@@ -0,0 +1,15 @@
+testcase = int(input())
+
+while (testcase):
+    lenght = int(input())
+
+    array = []
+
+    for i in range(lenght):
+        value = int(input())
+        array.append(value)
+
+    sub_lenght = 0
+
+    for i in range(lenght):
+        pass
