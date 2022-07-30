0480dada6851968ee0542ad17ec130362c52d893
Sudhanwa Kaveeshwar <sudhanwa.kaveeshwar@gamil.com>
2022-07-31 01:22:41 +0530

    bug in here

--- CPP/searches/BubbleSearch.cpp
+++ CPP/searches/BubbleSearch.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-// ! under trial trying to return exection time and sorted array 
+// ! under trial trying to return exection time and sorted array
 
 // struct ANSWER
 // {
@@ -11,17 +11,41 @@ using namespace std;
 //     int arr;
 // };
 
-void bubble(int arr[])
+
+
+int bubble(int arr[])
 {
     int sizeOfArray = sizeof(arr) / sizeof(arr[0]);
 
-    for (int i = 0; i < sizeOfArray;i++)
+    for (int i = 0; i < sizeOfArray; i++)
     {
-        
+        for (int j = 0; j = sizeOfArray - i - 1; j++)
+        {
+            if (arr[j] >= arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
     }
+
+    return arr;
 }
 
 int main()
 {
+    int arr[10] = {12, 41, 53, 6, 73, 8, 56, 66, 11, 34};
+
+    int *SortedArr = bubble(arr);
+
+    int sizeOfArray = sizeof(arr) / sizeof(arr[0]);
+    cout << sizeOfArray << endl;
+
+    for (int i = 0; i < sizeOfArray; i++)
+    {
+        cout << SortedArr[i] << " ";
+    }
+
     return 0;
 }
